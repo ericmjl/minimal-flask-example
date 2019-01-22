165fb897b2d93b60209a5e046482283f454fa7a0
Eric Ma <ericmajinglong@gmail.com>
2019-01-22 16:51:55 -0500

    Applied black to the code

--- app.py
+++ app.py
@@ -6,37 +6,43 @@ import bokeh
 import pandas as pd
 from util import make_plot
 
-df = pd.read_csv('https://data.boston.gov/dataset/c8b8ef8c-dd31-4e4e-bf19-af7e4e0d7f36/resource/29e74884-a777-4242-9fcc-c30aaaf3fb10/download/economic-indicators.csv',
-                 parse_dates=[['Year', 'Month']])
+df = pd.read_csv(
+    "https://data.boston.gov/dataset/c8b8ef8c-dd31-4e4e-bf19-af7e4e0d7f36/resource/29e74884-a777-4242-9fcc-c30aaaf3fb10/download/economic-indicators.csv",
+    parse_dates=[["Year", "Month"]],
+)
 length = len(df)
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html.j2')
+    return render_template("index.html.j2")
 
 
-@app.route('/bokehplot')
+@app.route("/bokehplot")
 def bokehplot():
     figure = make_plot()
     fig_script, fig_div = components(figure)
-    return render_template('bokeh.html.j2', fig_script=fig_script, fig_div=fig_div,
-                           bkversion=bokeh.__version__)
+    return render_template(
+        "bokeh.html.j2",
+        fig_script=fig_script,
+        fig_div=fig_div,
+        bkversion=bokeh.__version__,
+    )
 
 
-@app.route('/df')
+@app.route("/df")
 def dataframe():
-    return render_template('df.html.j2', length=length, dataframe=df.to_html())
+    return render_template("df.html.j2", length=length, dataframe=df.to_html())
 
 
-@app.route('/dfcustom')
+@app.route("/dfcustom")
 def dfcustom():
-    data = df.to_dict(orient='records')
+    data = df.to_dict(orient="records")
     headers = df.columns
     print(headers)
-    return render_template('dfcustom.html.j2', data=data, headers=headers)
+    return render_template("dfcustom.html.j2", data=data, headers=headers)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=5957)
--- util.py
+++ util.py
@@ -3,14 +3,19 @@ from bokeh.plotting import figure
 
 
 def make_plot():
-    colormap = {'setosa': 'red', 'versicolor': 'green', 'virginica': 'blue'}
-    colors = [colormap[x] for x in flowers['species']]
+    colormap = {"setosa": "red", "versicolor": "green", "virginica": "blue"}
+    colors = [colormap[x] for x in flowers["species"]]
 
-    p = figure(title = "Iris Morphology")
-    p.xaxis.axis_label = 'Petal Length'
-    p.yaxis.axis_label = 'Petal Width'
+    p = figure(title="Iris Morphology")
+    p.xaxis.axis_label = "Petal Length"
+    p.yaxis.axis_label = "Petal Width"
 
-    p.circle(flowers["petal_length"], flowers["petal_width"],
-             color=colors, fill_alpha=0.2, size=10)
+    p.circle(
+        flowers["petal_length"],
+        flowers["petal_width"],
+        color=colors,
+        fill_alpha=0.2,
+        size=10,
+    )
 
     return p
